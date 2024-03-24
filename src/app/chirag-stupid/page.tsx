0576ae6c2bf264e28a7cf34abf1a19a10739ca76
yuheiarimoto616 <yuhei61627@icloud.com>
2024-03-24 04:07:46 -0700

    deleted some unnecesssary folders

--- src/app/chirag-stupid/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Chirag() {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>Chirag</h1>
-      </main>
-    );
-  }
-  
