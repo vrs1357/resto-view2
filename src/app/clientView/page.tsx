98b155c10c2eaefbb26ebdbe0c5f0cdcb97c3056
yuheiarimoto616 <yuhei61627@icloud.com>
2024-03-24 12:21:31 -0700

    fixed centering issue

--- src/app/clientView/page.tsx
+++ src/app/clientView/page.tsx
@@ -343,7 +343,7 @@ export default async function restaurantView() {
   const graph2 = await createBarChart(1);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-12">
+    <main className="flex min-h-screen flex-col items-center justify-between p-12 m-auto">
       <div className="flex flex-col">
         {/* PAGE HEADER */}
         <div className="flex gap-5 max-md:flex-wrap">
